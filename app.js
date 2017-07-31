53057843fbcf47dd7f7460465d93ed3dd78c869c
Tristan Navarrete <tristan@silentblast.com>
2017-07-31 21:29:57 +0000

    Hello World!

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 const express = require('express');
-const pug = require('pug');
+const app = express();
 
-var app = express.app();
+const pug = require('pug');
+var path = require('path');
+//var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+var port = 8080;
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!')
+})
